1683c365c78d526fb581afaa476f7497d8a40f82
skeldoor <matthewdowns95@gmail.com>
2023-02-21 23:15:05 +0000

    Get stuff into git to continue working

--- src/main/java/com/example/ExamplePlugin.java
+++ src/main/java/com/example/ExamplePlugin.java
@@ -4,15 +4,21 @@ import com.google.inject.Provides;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.*;
+import net.runelite.api.events.AnimationChanged;
+import net.runelite.api.events.ClientTick;
 import net.runelite.api.events.GameStateChanged;
 import net.runelite.api.events.GameTick;
+import net.runelite.api.kit.KitType;
+import net.runelite.api.widgets.Widget;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 
 import java.lang.reflect.Field;
+import java.util.Arrays;
 import java.util.Objects;
+import java.util.stream.IntStream;
 
 @Slf4j
 @PluginDescriptor(
@@ -26,41 +32,70 @@ public class ExamplePlugin extends Plugin
 	@Inject
 	private ExampleConfig config;
 
-	@Override
-	protected void startUp() throws Exception
-	{
-		log.info("Example started!");
-	}
+	// Bronze Iron Steel Black Mithril Adamant Rune White Dragon Gilded
+	int[] rune2hItemId = {1307, 1309, 1311, 1313, 1315, 1317, 1319, 6609, 7158, 20155};
 
-	@Override
-	protected void shutDown() throws Exception
-	{
-		log.info("Example stopped!");
-	}
+	int superCool2hAnimationIdIdle = 7053;
+	int superCool2hAnimationIdIdleRotateLeft = 7044;
+	int superCool2hAnimationIdIdleRotateRight = 7044;
 
-	@Subscribe
-	public void onGameStateChanged(GameStateChanged gameStateChanged)
+	int superCool2hAnimationIdWalking = 7052;
+	int superCool2hAnimationIdWalkingRotateLeft = 7048;
+	int superCool2hAnimationIdWalkingRotateRight = 4047;
+
+	int superCool2hAnimationIdRunning = 7043;
+
+	@Subscribe(priority = -1000.0f)
+	public void onClientTick(ClientTick e)
 	{
-		if (gameStateChanged.getGameState() == GameState.LOGGED_IN) {
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.catName(), null);
+		Player local = client.getLocalPlayer();
+		if (client.getGameState() != GameState.LOGGED_IN) return;
+		if (local == null) return;
+		PlayerComposition localComp = local.getPlayerComposition();
+		if (localComp == null) return;
+		int weaponId = localComp.getEquipmentId(KitType.WEAPON);
+		client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "weaponId says " + weaponId, null);
+		if (IntStream.of(rune2hItemId).anyMatch(x -> x == weaponId)){
+			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "weaponId says yes", null);
+			swapPlayerAnimation(local);
+		} else{
+			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "weaponId says no", null);
 		}
 	}
 
-	@Subscribe
-	public void onGameTick(GameTick gameTick){
-		// Replace follower name
-		NPC follower = client.getFollower();
-		if (follower != null &&
-			!Objects.equals(follower.getName(), config.catName()) &&
-			!Objects.equals(config.catName(), "")){
-			NPCComposition followerComp = follower.getComposition();
-			tryReplaceName(followerComp, follower.getName(), config.catName());
-		}
-
-		// Replace chatbox name
-
-
-	}
+//	@Subscribe
+//	public void onGameTick(GameTick gameTick){
+//		// Replace follower name
+//		NPC follower = client.getFollower();
+//		if (follower != null &&
+//			!Objects.equals(follower.getName(), config.catName()) &&
+//			!Objects.equals(config.catName(), "")){
+//			NPCComposition followerComp = follower.getComposition();
+//			tryReplaceName(followerComp, follower.getName(), config.catName());
+//		}
+//
+//		// Replace chat box name
+//		int MyChatboxText = 14221318;
+//		int PetChatboxName = 15138820;
+//
+//		// Replace cat specific conversation
+//		Widget MyChatboxWidget = client.getWidget(MyChatboxText);
+//		if (MyChatboxWidget != null && Objects.equals(MyChatboxWidget.getText(), "Hey cat. What's up?")){
+//			MyChatboxWidget.setText("Hey " + config.catName() + ". What's up?");
+//		}
+//
+//		// Replace pet's chat box name
+//		// Override a cat's in-game name too so that you can have a name longer than 6 characters
+//		Widget PetChatboxWidget = client.getWidget(PetChatboxName);
+//		if (PetChatboxWidget != null && !Objects.equals(PetChatboxWidget.getText(), config.catName())){
+//			PetChatboxWidget.setText(config.catName());
+//		}
+//	}
+//
+//	String getPetName(int petId){
+//
+//		return null;
+//	}
 
 	@Provides
 	ExampleConfig provideConfig(ConfigManager configManager)
